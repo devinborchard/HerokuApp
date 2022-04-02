c6512173de117c13bddeb30ebc98637955b41337
Devin <devinborchard@gmail.com>
2022-04-02 16:40:59 -0400

    more dugging

--- index.js
+++ index.js
@@ -14,8 +14,15 @@ app.get('/api/customers', cors(), (req, res) => {
 });
 
 
-const path = require("path");
-app.use(express.static(path.join(__dirname, 'build')));
+if (process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "development" ||
+  process.env.NODE_ENV === "integration") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
