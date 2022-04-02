26207950c650a1d4ed48a61f3df33038fa0e85a2
Devin <devinborchard@gmail.com>
2022-04-02 16:19:44 -0400

    more dugging

--- server.js
+++ server.js
@@ -13,17 +13,18 @@ app.get('/api/customers', cors(), (req, res) => {
   res.json(customers);
 });
 
-const port = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === 'production') {
-  // Exprees will serve up production assets
-  app.use(express.static('client/build'));
-
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+if (process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "development" ||
+  process.env.NODE_ENV === "integration") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+const PORT = process.env.PORT || 5000;
+
+console.log(PORT);
+const server = app.listen(PORT);
