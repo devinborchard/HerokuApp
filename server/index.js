4e5e2d190ffa39e4492bd558d5a2d098793ad4e1
Devin <devinborchard@gmail.com>
2022-04-02 17:34:04 -0400

    rebase

--- server/index.js
+++ server/index.js
@@ -14,14 +14,13 @@ app.get('/api/customers', cors(), (req, res) => {
   res.json(customers);
 });
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
 if (process.env.NODE_ENV === "production" ||
   process.env.NODE_ENV === "development" ||
   process.env.NODE_ENV === "integration") {
   app.use(express.static("client/build"));
-
+  const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
