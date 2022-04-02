7e96607885e72494e7f2548e4789e8e27eb47a9e
Devin <devinborchard@gmail.com>
2022-04-02 17:39:02 -0400

    more dugging

--- server.js
+++ server.js
@@ -13,6 +13,16 @@ app.get('/api/customers', cors(), (req, res) => {
   res.json(customers);
 });
 
+if (process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "development" ||
+  process.env.NODE_ENV === "integration") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => `Server running on port ${PORT}`);
