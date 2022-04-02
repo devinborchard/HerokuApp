9a966c3cfa800cc09af3aa7c732739df882a3a56
Devin <devinborchard@gmail.com>
2022-04-02 17:35:54 -0400

    rebase

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ app.get('/api/customers', cors(), (req, res) => {
   res.json(customers);
 });
 
-const port = 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(port, () => `Server running on port ${port}`);
+app.listen(PORT, () => `Server running on port ${PORT}`);
