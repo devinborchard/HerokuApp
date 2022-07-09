118008e969cd5d6e9458eb0c4c026ae9c3fcae33
Devin <devinborchard@gmail.com>
2022-07-09 00:35:56 -0400

    setup

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+require("dotenv").config()
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 app.listen(port, function(){
-  console.log("server started")
+  console.log("server started on ",process.env.ENVIRONMENT, port)
 });
 
