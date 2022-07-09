df3e04602c0c25b04ef1813b4e5478b3e75a9bf8
Devin <devinborchard@gmail.com>
2022-07-09 00:45:14 -0400

    added route

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ require("dotenv").config()
 
 const app = express();
 
+app.get('/api', (req,res) => {
+  console.log('PAYLOAD RECEIVED: ', req)
+})
+
+
 const port = process.env.PORT || 5000;
 app.listen(port, function(){
   console.log("server started on ",process.env.ENVIRONMENT, port)
